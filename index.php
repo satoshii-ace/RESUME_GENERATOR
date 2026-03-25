166c59819169c37726768834f0349e71b3578977
Satoshi <148063396+satoshii-ace@users.noreply.github.com>
2026-03-26 00:25:22 +0800

    Add navigation, hero section, and favicon to index.php; include CSS styles

--- index.php
+++ index.php
@@ -3,12 +3,50 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Resume Generator</title>
     <link rel="stylesheet" href="./assets/css/index.css">
+    <link rel="icon" type="image/svg+xml" href="./assets/icons/favicon.svg">
 </head>
 <body>
-    <h1>HOME</h1>
-   
+    <nav>
+        <div class="title">
+            <h1>RESUME GENERATOR</h1>
+        </div>
+        <ul class="nav-links">
+            <li><a href="#hero">Home</a></li>
+            <li><a href="#generate">Generate</a></li>
+            <li><a href="#contacts">Contacts</a></li>
+        </ul>
+    </nav>
+
+    <section class="hero" id="hero">
+        <h1>Build Your Professional Resume in Minutes</h1>
+        <div class="hero-btn">
+            <button class="resume">Create My CV Now</button>
+            <button class="call">Keep in Touch</button>
+        </div>
+    </section>
+    <section class="generate" id="generate">
+
+    </section>
+    <section class="contacts" id="contacts">
+
+    </section>
+    <footer>
+
+    </footer>
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     <form action="./view/displayInfo.php" method="POST" enctype="multipart/form-data">
         <input type="text" placeholder="Input your name" name="name"><br>
         <input type="number" placeholder="Input your age" name="age"><br>
@@ -16,8 +54,8 @@
         <input type="submit" value="submit" name="submit">
     </form>
 
-    <!-- <a href="./view/displayInfo.php?data=hello kalibangon">Go to display</a> -->
+
 
 </body>
-<script src="./assests/js/index.js"></script>
+<script src="./assets/js/index.js"></script>
 </html>
